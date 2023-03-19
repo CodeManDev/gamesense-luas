0840685ae8c03cbe407ac976e61b1449824b0d16
CodeMan <56723319+CodeManDev@users.noreply.github.com>
2023-03-19 20:26:35 +0100

    Add files via upload

---
+++ dt_teleport.lua
@@ -0,0 +1,29 @@
+local master_switch = ui.new_checkbox("AA", "Other", "Double tap teleport with onshot AA")
+
+local double_tap_ref = { ui.reference("RAGE", "Aimbot", "Double tap") }
+local onshot_aa_ref = { ui.reference("AA", "Other", "On shot anti-aim") }
+
+toggled_onshot_aa = false
+next_time = 0
+
+local function on_paint_ui()
+    local double_tap = ui.get(double_tap_ref[1]) and ui.get(double_tap_ref[2])
+    local onshot_aa = ui.get(onshot_aa_ref[1]) and ui.get(onshot_aa_ref[2])
+
+    if double_tap then
+        ui.set(onshot_aa_ref[1], false)
+        toggled_onshot_aa = true
+        next_time = globals.curtime() + 0.1
+    elseif toggled_onshot_aa and next_time < globals.curtime() then
+        ui.set(onshot_aa_ref[1], true)
+        toggled_onshot_aa = false
+    end
+end
+
+client.set_event_callback("paint_ui", on_paint_ui)
+
+ui.set_callback(master_switch, function()
+    func = ui.get(master_switch) and client.set_event_callback or client.unset_event_callback
+
+    func("paint_ui", on_paint_ui)
+end)
